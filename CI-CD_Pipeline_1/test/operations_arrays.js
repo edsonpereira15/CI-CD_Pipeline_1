5c236225ca681d77feb2fbf23b0bbbe5bf7e88b7
edson.pereira15 <edson.pereira15@fatec.sp.gov.br>
2025-03-06 16:58:38 -0300

    Upd: Atualização da exibição do script de teste

--- CI-CD_Pipeline_1/test/operations_arrays.js
+++ CI-CD_Pipeline_1/test/operations_arrays.js
@@ -10,7 +10,7 @@ for (i = 0; i< A.length &&  i < B.length; i++){
   C[i] = A[i] + B[i];
 }
 
-window.alert("As somas dos elementos presentes em A e e B são:"+'\n'C);
+window.alert("As somas dos elementos presentes em A e e B são:"+'\n'+C);
 
 //Exec 5
 
